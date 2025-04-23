f0718cd7b4725daeeb6b312912ac0147de0d0989
Neusbataller <nebaib@alumnatflorida.es>
2025-04-23 13:05:16 +0200

    Cambios

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const cors = require("cors");
+
+app.set("port", process.env.PORT || 10000);
+
+// Middleware
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+// Rutas
+app.use(require("./routes/index"));
+
+app.listen(app.get("port"), () => {
+  console.log("hola desde el puerto: " + app.get("port"));
+});
